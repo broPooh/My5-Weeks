6f4071a658a145908b52ed46bbc875325d2ba31d
broMac <yhs4705@naver.com>
2022-05-07 11:41:43 +0900

    Alamofire 코드 작성

--- My5 Weeks/ViewController.swift
+++ My5 Weeks/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        getCurrentWeather()
     }
 
 
+    func getCurrentWeather() {
+        
+        
+        let url = "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=9980ef6473d22c2ad12a3def35fb099d"
+        
+        AF.request(url, method: .get).validate().responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("JSON: \(json)")
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
 }
 
